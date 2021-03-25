6108d241db8debe64cffb26e629c29ab985fd09a
Eduardo-Moreno <e_moreno23@hotmail.com>
2021-03-25 01:56:59 +0000

    Añadiendo funciones #3

--- src/mex/simplex/problem_definition.py
+++ src/mex/simplex/problem_definition.py
@@ -1,137 +1,90 @@
 import numpy as np
 
-def create_matrix(variables, constraints):
-    """
-    Creates a matrix with enough rows for each constraint plus the objective function
-    and enough columns for all the variables.
-    Args:
-        - variables (int): number of variables
-        - constraints (int): number of constraints
-    Returns:
-        - matrix (numpy array)
-    """
-    
-    matrix = np.zeros((constraints + 1, variables + constraints + 2))
-    
-    return matrix
+from src.mex.utils.general import gen_var, convert
+from src.mex.simplex.simplex import next_round_r, loc_piv_r, pivot, next_round, loc_piv, convert_min
 
 
-def pivots_col(matrix):
+def add_cons(table):
     """
-    Checks to see if pivots are required due to negative values in right column,
-    excluding the bottom value
+    Check if 1 extra constraints can be added to the matrix, this means that there are at least two rows of all 0 elements. If this condition is not satisfied, our program will not allow the user to add additional constraints.
     """
     
-    m = min(matrix[:-1,-1])
-    if m >= 0:
-        return False
-    else:
+    lr = len(table[:,0])
+    empty = []
+    
+    for i in range(lr):
+        total = 0
+        for j in table[i,:]:
+            total += j**2
+        if total == 0:
+            empty.append(total)
+    
+    if len(empty)>1:
         return True
-
-
-def pivots_row(matrix):
-    """
-    Checks to see if pivots are required due to negative values in bottom row, 
-    excluding the final value
-    """
-    
-    l = len(matrix[:,0])
-    m = min(matrix[l-1,:-1])
-    if m >= 0:
+    else:
         return False
+    
+    
+def constrain(table, eq):
+    """
+    Add constraints to the problem.
+    """
+    if add_cons(table):
+        lc = len(table[0,:])
+        lr = len(table[:,0])
+        var = lc - lr -1
+        j = 0
+        while j < lr:
+            row_check = table[j,:]
+            total = 0
+            for i in row_check:
+                total += float(i**2)
+            if total == 0:
+                row = row_check
+                break
+            j +=1
+        eq = convert(eq)
+        i = 0
+        while i<len(eq)-1:
+            row[i] = eq[i]
+            i +=1
+        row[-1] = eq[-1]
+        row[var+j] = 1
     else:
-        return True
-
-
-def find_negative_col(matrix):
-    """
-    Finds location of negative values in right column
-    """
-    
-    l = len(matrix[0,:])
-    m = min(matrix[:-1,l-1])
-    if m<= 0:
-        n = np.where(matrix[:-1,l-1] == m)[0][0]
-    else:
-        n = None
-    
-    return n
-
-
-def find_negative_row(matrix):
-    """
-    Finds location of negative values in bottom row
-    """
-    
-    l = len(matrix[:,0])
-    m = min(matrix[l-1,:-1])
-    if m <= 0:
-        n = np.where(matrix[l-1,:-1] == m)[0][0]
-    else:
-        n = None
-    
-    return n
-
-
-def find_pivot_col(matrix):
-    """
-    Finds pivot element corresponding to a negative value in right column
-    """
-    
-    total = []
-    neg = find_negative_col(matrix)
-    row = matrix[neg,:-1]
-    m = min(row)
-    c = np.where(row == m)[0][0]
-    col = matrix[:-1,c]
-    for i,j in zip(col, matrix[:-1,-1]):  #i for col with neg, j for right col
-        if i != 0 and j/i > 0:
-            total.append(j/i)
-        else:
-            total.append(10000) #placeholder, might need to update for large scale
-    index = total.index(min(total))
-    
-    return [index,c]
-
-
-def find_pivot_row(matrix):
-    """
-    Finds pivot element corresponding to a negative value in bottom row
-    """
-    
-    if pivots_row(matrix):
-        total = []
-        neg = find_negative_row(matrix)
-        for i,j in zip(matrix[:-1,neg],matrix[:-1,-1]):
-            if i != 0 and j/i > 0:
-                total.append(j/i)
-            else:
-                total.append(10000) #placeholder, might need to update for large scale
-        index = total.index(min(total))
+        print('Cannot add another constraint.')
         
-        return [index,neg]
 
-
-def pivot(row,col,matrix):
+def add_obj(table):
     """
-    Pivot about a value to remove negative in final column or row
+    Verify if the objective function can be added.
     """
-    
-    lr = len(matrix[:,0])
-    lc = len(matrix[0,:])
-    t = np.zeros((lr,lc))
-    pr = matrix[row,:]
-    if matrix[row,col]**2>0:
-        e = 1/matrix[row,col]
-        r = pr*e
-        for i in range(len(matrix[:,col])):
-            k = matrix[i,:]
-            c = matrix[i,col]
-            if list(k) == list(pr):
-                continue
-            else:
-                t[i,:] = list(k-r*c)
-        t[row,:] = list(r)
-        return t
+    lr = len(table[:,0])
+    empty = []
+    for i in range(lr):
+        total = 0
+        for j in table[i,:]:
+            total += j**2
+        if total == 0:
+            empty.append(total)
+    if len(empty)==1:
+        return True
     else:
-        print('Cannot pivot on this element')
+        return False
+
+
+def obj(table,eq):
+    """
+    Add the objective function to the tableau.
+    """
+    if add_obj(table):
+        eq = [float(i) for i in eq.split(',')]
+        lr = len(table[:,0])
+        row = table[lr-1,:]
+        i = 0
+        while i<len(eq)-1:
+            row[i] = eq[i]*-1
+            i +=1
+        row[-2] = 1
+        row[-1] = eq[-1]
+    else:
+        print('You must finish adding constraints before the objective function can be added.')
