d23222b23e3f1a8515163624337e694351cdeb82
Eduardo-Moreno <e_moreno23@hotmail.com>
2021-03-25 02:15:23 +0000

    Añadiendo funciones #3

--- src/mex/utils/general.py
+++ src/mex/utils/general.py
@@ -1,6 +1,6 @@
 def convert_min(table):
     """
-    If the problem to solve is maximization it is analogue to solve the problem -minimization. This function multiply by -1 the objective function for maximization problems.
+    If the problem to solve is maximization it is analogue to solve the problem -minimization. So this function multiply by -1 the objective function for maximization problems.
     """
     table[-1,:-2] = [-1*i for i in table[-1,:-2]]
     table[-1,-1] = -1*table[-1,-1]    
