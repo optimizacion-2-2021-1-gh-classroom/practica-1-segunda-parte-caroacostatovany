827882027b004da21d93c47775c0874840324011
caroacostatovany <carolina.acostatovany@gmail.com>
2021-03-25 00:27:23 -0600

    dummy modification to test github actions

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -39,6 +39,7 @@ extensions = ["sphinx.ext.autodoc", # Core Sphinx library for auto html doc gene
 ]
 
 autosummary_generate = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
