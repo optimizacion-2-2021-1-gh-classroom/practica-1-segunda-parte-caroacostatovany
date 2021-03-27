bb1b00f06747fd7797ac2129b11070157cb56e81
caroacostatovany <carolina.acostatovany@gmail.com>
2021-03-27 16:20:51 -0600

    Remove .ipynb_checkpoints and .gitkeep files

--- src/mex/utils/.ipynb_checkpoints/general-checkpoint.py
+++
@@ -1,7 +0,0 @@
-def convert_min(table):
-    """
-    If the problem to solve is maximization it is analogue to solve the problem -minimization. So this function multiply by -1 the objective function for maximization problems.
-    """
-    table[-1,:-2] = [-1*i for i in table[-1,:-2]]
-    table[-1,-1] = -1*table[-1,-1]    
-    return table
