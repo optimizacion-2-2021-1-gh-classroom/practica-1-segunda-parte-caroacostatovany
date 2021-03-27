4a8f400cb840a302f2b87c126b133d572167018a
cecyar <cecyar@gmail.com>
2021-03-26 22:44:18 -0600

    Corrigiendo return de convert_min

--- src/mex/utils/general.py
+++ src/mex/utils/general.py
@@ -15,7 +15,7 @@ def convert_min(matrix):
     matrix[-1, :-2] = [-1*i for i in matrix[-1, :-2]]
     matrix[-1, -1] = -1*matrix[-1, -1]
     
-    return table
+    return matrix
 
 
 def gen_var(matrix):
