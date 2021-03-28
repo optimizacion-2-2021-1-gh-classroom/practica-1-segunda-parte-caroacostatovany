4c2554699a3cef3cd24c9fecd626e6f056678a83
Carolina Acosta Tovany <carolina.acostatovany@gmail.com>
2021-03-27 21:44:05 -0600

    add installations

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b master --single-branch https://github.com/optimizacion-2-2021-1-gh-classroom/practica-1-segunda-parte-caroacostatovany.git
+cd ${HOME} && pip install matplotlib && pip install scipy && pip install networkx && git clone -b master --single-branch https://github.com/optimizacion-2-2021-1-gh-classroom/practica-1-segunda-parte-caroacostatovany.git
 
 exec "$@"
