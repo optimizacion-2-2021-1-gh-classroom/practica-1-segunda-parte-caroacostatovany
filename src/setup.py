0396c6522c908d8d0e4cc21f1c042b8d030836c9
caroacostatovany <carolina.acostatovany@gmail.com>
2021-04-10 11:25:13 -0500

    Quitar paquete nose y agregar requirements.txt

--- src/setup.py
+++ src/setup.py
@@ -12,7 +12,6 @@ setup(name="mex",
       install_requires = [
                           "numpy",
                           "pandas",
-                          "nose",
                           "sphinx"
                           ],
       )
